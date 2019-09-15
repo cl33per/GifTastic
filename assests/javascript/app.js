0ba3375016bc9598676d1216c02bfc1b66213f34
cl33per <leeper08@gmail.com>
2019-09-15 13:00:54 -0400

    added meta data and fixed css

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
+
+  // Document Listens for classes to fire off functions
   $(document).on("click", ".giphy-btn", displayGiphyAwesome);
-  $(document).on("click", ".gif-img", test);
+  $(document).on("click", ".gif-img", stillGif);
+
+  // Starting Array for document buttons
   var topics = ['Capatin Marvel', 'Thanos', 'Thor', 'Tony Stark', 'Winter Solider', 'Spider Man', 'Death', 'Wonder Woman', 'Super Man'];
 
+  // Function to render the array above as starting buttons
   function renderButtons() {
     $("#buttons-view").empty();
     for (var i = 0; i < topics.length; i++) {
@@ -14,49 +19,56 @@ $(document).ready(function () {
     };
   };
 
-  $("#add-giphy").on("click", function (event) {  
+  // Add an additonal button the exisiting array
+  $("#add-giphy").on("click", function (event) {    
     event.preventDefault();
     var gif = $("#giphy-input").val().trim();
-    topics.push(gif);
+    topics.push(gif);    
     renderButtons();
   });
 
 
-
-  renderButtons();
-
+  /* Conntects to giphy API and sets the  configuration for the rest of the application, loads gifs with still image as its priary source, and loads the image with attributes of both the still and moving urls 
+   */
+  
   function displayGiphyAwesome() {
-
     var limit = "10"
-    var rating = "";
+    var rating = "";  
     var searchQuery = $(this).attr("data-name");
     var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=03ykwZRIcUljk2ki7d4lwshpGBOYOHvc&q=' + searchQuery + ' &limit=' + limit + '&offset=0&rating=' + rating + '&lang=en';
+
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function (response) {
       var results = response.data;
-      console.log(results);
       for (var i = 0; i < results.length; i++) {
-        var giphyDiv = $("<div class='flex-wrap'>")
+        var giphyDiv = $("<div class='row flex-wrap bg-light my-5 mx-5'>")
         var giphyImage = $("<img>");
-        var p = $("<p>").text("Rating: " + results[i].rating);
+        var giphyInfo = $("<div class='col-lg-6'>")
+        var p1 = $("<p>").text("Rating: " + results[i].rating);
+        var p2 = $("<p>").text("Title: " + results[i].title);
+        var p3 = $("<p>").text("Created: " + results[i].import_datetime);
         giphyImage.attr("class", 'gif-img')
         giphyImage.attr("src", results[i].images.fixed_height_still.url);
         giphyImage.attr("data-still", results[i].images.fixed_height_still.url);
         giphyImage.attr("data-animate", results[i].images.fixed_height.url);
         giphyImage.attr("data-state", 'still');
         giphyDiv.append(giphyImage);
-        giphyDiv.append(p);
-        
+        giphyInfo.append(p1);
+        giphyInfo.append(p2);
+        giphyInfo.append(p3);
+        giphyDiv.append(giphyInfo);
         $("#giphy").prepend(giphyDiv);
       }
     });
   };
 
-  function test() {
-    console.log('click works');
+  /* Conditional for loaded gifs, if the gifs are in the data-state = still when clicked the state changes to data-state = animate then the src is changed to the animated src.
+   */
+  function stillGif() {
     var state = $(this).attr("data-state");
+    
     if (state === "still") {
       $(this).attr("src", $(this).attr("data-animate"));
       $(this).attr("data-state", "animate");
@@ -65,4 +77,13 @@ $(document).ready(function () {
       $(this).attr("data-state", "still");
     };
   };
+
+  // Function to add 10 more additonal gif to the exsiting query
+  $("#add-more").on("click", function () {
+    
+  });
+
+  // Fires renderButtons Function
+  renderButtons();
+
 });
